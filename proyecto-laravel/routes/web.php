6573c8b4df273a7438e71d70ed5c6efc9699403f
Enmanuel Cruz <Enmanuelc3197@gmail.com>
2025-10-02 18:41:09 -0400

    Se agrego una ruta llamada animales para realizar una prueba

--- proyecto-laravel/routes/web.php
+++ proyecto-laravel/routes/web.php
@@ -5,3 +5,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/welcome', function () {
     return view('welcome');
 });
+
+Route::get('/Pruebas', function () {
+    return '<h1>Hola Mundo con Laravel</h1>';
+});
+
+Route::get('/animales', 'App\Http\Controllers\PruebasController@index');
